207a8a14e978129a1e2ed44184030b1c0ca754ce
akhandelwal123 <abhishekakhandelwal@gmail.com>
2017-08-29 13:14:16 -0500

    most of the fields done one time

--- test.R
+++ test.R
@@ -1,30 +1,23 @@
-#4. 
-x <- "Lk's Marcella    67.5 1 1 2    2    1  .5  31.45 2.30 Bmpd, Kept To Task "
- z <- strsplit(x[[1]], " ")[[1]]
- y <- strsplit(z, " ")
- y[lapply(y,length)>0] 
- y <- Filter(length, y)
- paste(y[1],y[2])
-#a<-gsub(" ", ",", x, fixed=TRUE)
+x <- "5 Lee Banshee                       2.2 "
 
-# y[4]
-# y[10]
-# y[12]
-# y[13]
- 
-#length(y)
- print(!grepl("^[A-Za-z,]+$", y[12], perl = T))
- flag = TRUE
 
-for (i in length(y):length(y)-10) {
-    if (!grepl("^[A-Za-z,]+$", y[i], perl = T)) {
-      a <- y[i]
-      b <- y[i-1]
-      c <- y [i-3]
-      browse()
-    }
-  break
+z <- strsplit(x[[1]], " ")[[1]]
+y <- strsplit(z, " ")
+y[lapply(y,length)>0]
+y <- Filter(length, y)
+
+
+h <- rev(y)
+for (i in 1 : 10) {
+  if (!grepl("^[A-Za-z,&1st]+$", h[i], perl = T)) {
+    a <- h[i]
+    b <- h[i+1]
+    c <- h [i+2]
+    break
+  }
+
 }
 a
 b
 c
+
