4f9f11ba07eee629cb5d87aa8a232e5e6b3862f2
akhandelwal123 <abhishekakhandelwal@gmail.com>
2017-08-31 22:32:46 -0500

    Southland.R 8 elemnts covered

--- Southland.R
+++ Southland.R
@@ -4,6 +4,48 @@ library(xlsxjars)
 library(xlsx)
 library(WriteXLS)
 
+
+# function to filter the characters 
+filterChar <- function(c){
+  y <- unique(na.omit(unlist(strsplit(unlist(c), "[^a-zA-Z's]+"))))
+  z <- paste(y, collapse = ' ')
+  return (trimws(z))
+}
+
+handleFoRtFp <- function(revSecondSplit,FinishingPosition,Time,FinalOdds){
+  for (i in 1:10) {
+    #2    1  .5  31.45 2.30 Bmpd, Kept To TasK  ==> these are 10 elemnts of revSecondSplit
+    if (!grepl("^[A-Za-z,&1st\r]+$", revSecondSplit[i], perl = T)) {
+      fo <- revSecondSplit[i]
+      rt <- revSecondSplit[i + 1]
+      fp <- revSecondSplit [i + 3]
+      break
+    }
+  }
+  if (exists("fp") && exists("rt") && exists("fo")) {
+    assign("FinishingPosition",  c(FinishingPosition, fp), envir = .GlobalEnv)
+    assign("Time",  c(Time, rt), envir = .GlobalEnv)
+    assign("FinalOdds",  c(FinalOdds, fo), envir = .GlobalEnv)
+  }
+  
+}
+
+handleStartPos <- function(secondSplit,StartingPosition){
+  for(l in 3:6) {
+    if (nchar(secondSplit[l]) == 1){
+      assign("StartingPosition",  c(StartingPosition, secondSplit[l]), envir = .GlobalEnv)
+      break
+    }
+  }
+}
+
+handleName <- function(secondSplit,Name){
+  if (!(grepl('NULL' ,secondSplit[1]) | grepl("\\d", secondSplit[1]))) {
+    nameCol <- filterChar(c(secondSplit[1],secondSplit[2],secondSplit[3],secondSplit[4]))
+    assign("Name",  c(Name, nameCol), envir = .GlobalEnv) 
+  }
+}
+
 handleTrack <- function(trk) {
   trackhead <- tolower(trimws(trk))
   track <- get(trackhead,trackmap)
@@ -28,8 +70,8 @@ handleDistance <- function(x,RaceNumber,Distance,Grade){
   gd <- trimws(substr(x, 12, 13))
   dist <- trimws(substr(x, 24, 27))
   assign("RaceNumber",  c(RaceNumber, rn), envir = .GlobalEnv) 
-  assign("RaceNumber",  c(Distance, dist), envir = .GlobalEnv) 
-  assign("RaceNumber",  c(Grade, gd), envir = .GlobalEnv) 
+  assign("Distance",  c(Distance, dist), envir = .GlobalEnv) 
+  assign("Grade",  c(Grade, gd), envir = .GlobalEnv) 
 }
 
 #function to store racenumber to finalodds
@@ -65,7 +107,7 @@ assign('derby Lane', 'DE', trackmap)
 assign('Ebro', 'EB', trackmap)
 assign('Flagler', 'FL', trackmap)
 assign('Iowa', 'IO', trackmap)
-assign('Orange Park', 'OR', trackmap)
+assign('orange park', 'OR', trackmap)
 assign('Palm Beach', 'PB', trackmap)
 assign('Pensacola', 'PE', trackmap)
 assign('Sanford-Orlando', 'Sun', trackmap)
@@ -76,15 +118,22 @@ assign('Wheeling', 'WH', trackmap)
 temp <- c()
 Track <- c()
 RaceDate <- c()
-RaceNumber <-
-  Distance <-
-  Grade <- c()
+#Fileds in excel
+RaceNumber <-  Distance <-  Grade <-  Name <-  StartingPosition <-
+  FinishingPosition <- Time <- FinalOdds <- Win <-  Place <-
+  Show <- Quinella <- Perfecta <- Trifecta <- Superfecta <- Winplaceshow <- c()
 track = TRUE
 
+# file <-
+#   pdf_text('C://abhiimpdata//R//pdfs//SOUTHLAND-Aug24-Thursday-Twilight-Charts.pdf')
+# info <-
+#   pdf_info('C://abhiimpdata//R//pdfs//SOUTHLAND-Aug24-Thursday-Twilight-Charts.pdf')
+
 file <-
-  pdf_text('C://abhiimpdata//R//pdfs//SOUTHLAND-Aug24-Thursday-Twilight-Charts.pdf')
+  pdf_text('C://abhiimpdata//R//ORANGE-PARK-Aug18-Friday-Evening-Charts.pdf')
 info <-
-  pdf_info('C://abhiimpdata//R//pdfs//SOUTHLAND-Aug24-Thursday-Twilight-Charts.pdf')
+  pdf_info('C://abhiimpdata//R//ORANGE-PARK-Aug18-Friday-Evening-Charts.pdf')
+
 pages <- info$pages
 
 
@@ -114,13 +163,22 @@ for (k in 1:pages) {
   for (line in lines[-c(1,2)]) {
     x <- substr(line , 1 , limit / 2)
     if (!is.null(x) && !nchar(trimws(x)) == 0) {
-      if (grepl("[A-z]$", trimws(x))) {
+      if (grepl("[A-z]$", trimws(x)) && !grepl('-' , x , ignore.case = TRUE) && !grepl('/' , x , ignore.case = TRUE)) {
         # filter for main criteria
         if (grepl('Distance' , x, ignore.case = TRUE)) {
             handleDistance(x,RaceNumber,Distance,Grade)
         }
         else {
+          firstSplit <- strsplit(x[[1]], " ")[[1]]
+          secondSplit <- strsplit(firstSplit, " ")
+          secondSplit[lapply(secondSplit, length) > 0]
+          secondSplit <- Filter(length, secondSplit)
           
+          handleName(secondSplit,Name)
+          
+          handleStartPos(secondSplit,StartingPosition)
+          
+          handleFoRtFp(rev(secondSplit),FinishingPosition,Time,FinalOdds)
         }
       }
       else {
